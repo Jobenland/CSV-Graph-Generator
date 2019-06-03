d50349e61337a7d0e9d3fdadbf2b91dfe26cd9c0
Jonathan Obenland <jobenland1@gmail.com>
2019-06-03 12:56:56 -0400

    Added header selection for x and y axis

--- Grapher.py
+++ Grapher.py
@@ -9,7 +9,7 @@ __author__ = "Jonathan Obenland"
 __copyright__ = "Copyright 2019, MEII"
 __credits__ = ["Jonathan Obenland", "Mikethewatchguy"]
 __license__ = "GPL"
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __maintainer__ = "Jonathan Obenland"
 __email__ = "jobenland1@gmail.com"
 __status__ = "Production"
@@ -19,12 +19,15 @@ import csv
 import pandas as pd
 import pandas
 import sys
+import np
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import ColumnDataSource
 if sys.version_info[0] >= 3:
     import PySimpleGUI as sg
 else:
     import PySimpleGUI27 as sg
+
+
 
 #Main method that runs all features of the program 
 def Main():
@@ -50,23 +53,25 @@ def Main():
 
     #sets the preview pane to see the preview portion
     Preview = [ [sg.Menu(menu_def)],
-               [sg.Column(columm_layout, size=(300,100), scrollable=True)] ]
-    #TODO add new tool that will list all the available columns and the user can go through and select the ones to include
+               [sg.Column(columm_layout, size=(300,100), scrollable=True)]]
     #sets the layout for the graph settings on the box
     Setting = [[sg.Slider(range=(1,1000), default_value=500, size=(10,10), orientation='horizontal', key = 'height',font=('Helvetica', 12)),
-                    sg.Text('    Name:  ', size=(10,1)), sg.InputText(key='graphtitle', size=(15,1))],
+                    sg.Text('    Name: ', size=(10,1)), sg.InputText(key='graphtitle', size=(15,1))],
                [sg.Text('Enter graph Height')],
                [sg.Slider(range=(1,1000), default_value=500, size=(10,10), orientation='horizontal', key = 'width', font=('Helvetica', 12)),
-                    sg.Text('       '),sg.InputCombo(['Red', 'Green', 'Blue', 'Yellow', 'Orange']),sg.Text('   Legend Location  '), sg.InputCombo(['Top Left','Top Right','Bottom Left', 'Bottom Right'], key = 'legendloc')],
-               [sg.Text('Enter graph Width')]]
+                    sg.Text('       '),sg.Text(' Legend Location  '), sg.InputCombo(['Top Left','Top Right','Bottom Left', 'Bottom Right'], key = 'legendloc')],
+               [sg.Text('Enter graph Width')],
+               [sg.Text(' ')],
+               [sg.Text('Select the X axis'), sg.Text('                              Select the y axis(s)')],
+               [sg.Listbox(['Load CSV to See available headers'], key = 'xheaders', size=(30,6)),sg.Listbox(['Load CSV to See available headers'],select_mode='multiple',key = 'yheaders', size=(30,6))]]
               
     #general layout bringing all the smaller frames together
     layout = [[sg.Text('use open to load the csv')], 
-              [sg.Frame('Preview', Preview, title_color='green', font = 'Any 12')],
+              [sg.Frame('Preview', Preview, title_color='green', font = 'Any 12'), sg.Image('UMD.png')],
               [sg.Frame('Graph Settings', Setting, title_color='blue', font = 'Any 12')]]
 
     #names the table and creates the layout
-    window = sg.Window('Table', return_keyboard_events=True).Layout(layout)
+    window = sg.Window('Table', return_keyboard_events=True).Layout(layout).Finalize()
 
     #starts the event listener for the window
     while True:
@@ -87,14 +92,38 @@ def Main():
 
             #opens up a window to choose a file with the extension .csv
             filename = sg.PopupGetFile('filename to open', no_window=True, file_types=(("CSV Files","*.csv"),))
+
+            #populates the first box for choosing the x axis
+            if filename is not None:
+                with open(filename, "r") as infile:
+                    #sets the headers to an array
+                    reader = csv.reader(infile)
+                    header_list=[]
+                    header_list = next(reader)
+                    print(header_list)
+                    #updates the box with the array of x headers
+                    headerupdate = window.FindElement('xheaders')
+                    headerupdate.Update(header_list)
+
+            #populates the secound box for choosing the y axis to include     
+            if filename is not None:
+                with open(filename, "r") as infile:
+                    #sets the headers to an array
+                    reader = csv.reader(infile)
+                    header_list2=[]
+                    header_list2 = next(reader)
+                    print(header_list)
+                    #updates the box with the new headers from the array
+                    headerupdate2 = window.FindElement('yheaders')
+                    headerupdate2.Update(header_list2)  
             
             #opens in read and populates var with everything in the csv
             if filename is not None:
                 with open(filename, "r") as infile:
                     reader = csv.reader(infile)
                     try:
+                        #read everything into rows
                         data = list(reader)
-                          # read everything else into a list of rows
 
                     #error handling to make sure that the file is readable
                     except:
@@ -155,46 +184,56 @@ def Main():
             #read the csv of the given filename
             weight = pd.read_csv(filename)
 
-            #TODO make this variable based off user input
-            xx=weight["time(hours)"]
-            yy1=weight["Electrode"]
-            yy2=weight["Ohmic"]
-            yy3=weight["tasr"]
+            #gets the values of the headers
+            xhead = values['xheaders']
+            yhead = values['yheaders']
 
-            #sets the name and adds html so launch in browser and display graph
-            output_file(name+'.html')
+            #array of colors to choose from
+            #TODO add more colors
+            colorar = ['green','blue','red','orange',]
+
+            #headers.Update(header_list)
             p = figure(title = name, plot_width=plotwidth, plot_height=plotheight)
-            p.circle(xx,yy1, legend="Electrode")
-            p.line(xx,yy1, legend="Electrode")
-            p.line(xx,yy2, legend="Ohmic", line_color='green')
-            p.square(xx,yy2, legend="Ohmic", fill_color='green',line_color='green')
-            p.line(xx,yy3, legend="Tasr", line_color='red')
-            p.diamond(xx,yy3, legend="Tasr", line_color='red', fill_color='red')
+            i=0
+            #TODO remove this is uneccessay 
+            #   you only need one x
+            for i in range(len(xhead)):
+                xx=weight[xhead[i]]
 
-
-            #TODO add ability for user to choose location
+            #goes through each of the headers selected and uses them in the program
+            for i in range(len(yhead)):
+                
+                #TODO optimize
+                ccolor = colorar[i]
+                print(ccolor)
+                #increments yy with a number at the end for adding more lines
+                var = 'yy'+str(i)
+                var = weight[yhead[i]]
+                print(var)
+                #creates the lines
+                p.circle(xx,var, legend=yhead[i],fill_color=ccolor,line_color=ccolor)
+                p.line(xx,var, legend=yhead[i],line_color=ccolor)
+                #testing...
+                print(yhead[i])
+            
+            output_file(name+'.html')
             lloc = values['legendloc']
-
+            
+            #user selected location of legend
             if lloc == 'Top Right':
-
                 p.legend.location = "top_right"
 
             elif lloc == 'Top Left':
-
                 p.legend.location = "top_left"
 
             elif lloc == 'Bottom Right':
-
                 p.legend.location = "bottom_right"
 
             elif lloc == 'Bottom Left':
-
                 p.legend.location ="bottom_left" 
-
             #show the graph
             show(p)
 
-        
         # if a valid table location entered, change that location's value
         try:
             location = (int(values['inputrow']), int(values['inputcol']))
