fc7d3fd1e8bcab2cb86ada2a2bd271f51a80bcbd
Jonathan Obenland <jobenland1@gmail.com>
2019-06-26 16:06:59 -0400

    Fixed Preview and Added Examples

--- Grapher.py
+++ Grapher.py
@@ -21,7 +21,7 @@ import sys
 import np
 import webbrowser
 from bokeh.plotting import figure, output_file, show
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource,HoverTool
 if sys.version_info[0] >= 3:
     import PySimpleGUI as sg
 else:
@@ -37,7 +37,7 @@ def Main():
 
     #adds the options to the bar at top
     menu_def = [['File', ['Open', 'Exit']],
-                ['Generate', ['Graph', ['line', 'bar',], 'Reset'],],
+                ['Generate', ['Preview','Graph', ['line', 'bar',], 'Reset'],],
                 ['Help', ['About...','Submit An Issue']]]
 
     columm_layout = [[]]
@@ -45,7 +45,8 @@ def Main():
     #creates the preview portion to look at the opened csv file
     MAX_ROWS = 800
     MAX_COL = 10
-
+    data=[]
+    header_lisst=[1,2,3,4,5,6,7]
     #ammends the columns and set up the key to utilize features of the preview tab
     for i in range(MAX_ROWS):
         inputs = [sg.T('{}'.format(i), size=(4,1), justification='right')] + [sg.In(size=(10, 1), pad=(1, 1), justification='right', key=(i,j), do_not_clear=True) for j in range(MAX_COL)]
@@ -53,7 +54,13 @@ def Main():
 
     #sets the preview pane to see the preview portion
     Preview = [ [sg.Menu(menu_def)],
-               [sg.Column(columm_layout, size=(410,100), scrollable=True)]]
+                [sg.Text('To preview imported data, click the button below    ')],
+                [sg.Text('                                                    ')],
+                [sg.Text('No CSV has been entered', size = (51,1),text_color = 'red',key = 'fn')],
+                [sg.Text('                                                        ')]]
+               #[sg.Column(columm_layout, size=(410,100), scrollable=True)],
+               #[sg.Table(values=data,max_col_width=1,headings=header_lisst,
+               #auto_size_columns=False, justification='right',alternating_row_color = 'lightblue',vertical_scroll_only=False,key='tab')]]
     #sets the layout for the graph settings on the box
     Setting = [[sg.Slider(range=(1,1500), default_value=610, size=(10,10), orientation='horizontal', key = 'height',font=('Helvetica', 12)),
                     sg.Text('    Name: ', size=(10,1)), sg.InputText(key='graphtitle', size=(15,1)), sg.Text('   Title of x-axis ', size = (14,1)),
@@ -68,36 +75,39 @@ def Main():
                [sg.Listbox(['Load CSV to See available headers'], key = 'xheaders', size=(30,6)),sg.Listbox(['Load CSV to See available headers'],select_mode='multiple',key = 'yheaders', size=(30,6)), sg.Checkbox('Maintain ASR',default = False,key='ASR')]]
               
     #general layout bringing all the smaller frames together
-    layout = [[sg.Text('use open to load the csv')], 
+    layout = [[sg.Text('First, Use Open to load a CSV into the program and verify the correct path in the preview box.')],
+              [sg.Text('Note to keep the box a perfect square, leave the height and width at 610 by 650')], 
               [sg.Frame('Preview', Preview, title_color='green', font = 'Any 12'), sg.Image('UMD.png')],
               [sg.Frame('Graph Settings', Setting, title_color='blue', font = 'Any 12')],
               [sg.Text('Property of Maryland Energy Innovation Institute                                           written by Jonathan Obenland', text_color = 'red')],
               [sg.Text('All rights reserved under GNU-GPL version 3                                                Python 3.x   Build: ', text_color = 'blue'),sg.Text("PASSING",text_color = 'green')]]
 
     #names the table and creates the layout
-    window = sg.Window('Table', return_keyboard_events=True).Layout(layout).Finalize()
+    window1 = sg.Window('Table', return_keyboard_events=True).Layout(layout).Finalize()
 
     #starts the event listener for the window
+    window2_active = False
     while True:
 
         #reads the window
-        event, values = window.Read()
+        event1, values1 = window1.Read()
         
         #if exit
-        if event is None or event == 'Exit':
+        if event1 is None or event1 == 'Exit':
             break
 
         #if they click about
-        elif event == 'About...':
+        elif event1 == 'About...':
             sg.Popup('A simple graphing program to plot various headers and points from a CSV')
 
         #if they click open
-        elif event == 'Open':
-
+        elif event1 == 'Open':
+            
             #opens up a window to choose a file with the extension .csv
             filename = sg.PopupGetFile('filename to open', no_window=True, file_types=(("CSV Files","*.csv"),))
 
-        
+            nameUpdate = window1.FindElement('fn')
+            nameUpdate.Update(filename, text_color = 'green')
 
             #populates the first box for choosing the x axis
             if filename is not None:
@@ -108,7 +118,7 @@ def Main():
                     header_list = next(reader)
                     print(header_list)
                     #updates the box with the array of x headers
-                    headerupdate = window.FindElement('xheaders')
+                    headerupdate = window1.FindElement('xheaders')
                     headerupdate.Update(header_list)
 
             #populates the secound box for choosing the y axis to include     
@@ -120,7 +130,7 @@ def Main():
                     header_list2 = next(reader)
                     print(header_list)
                     #updates the box with the new headers from the array
-                    headerupdate2 = window.FindElement('yheaders')
+                    headerupdate2 = window1.FindElement('yheaders')
                     headerupdate2.Update(header_list2)  
             
             #opens in read and populates var with everything in the csv
@@ -138,6 +148,19 @@ def Main():
                         sg.PopupError('Error reading file')
                         continue
                 '''
+                if filename is not None:
+                    with open(filename,'r') as infile:
+                        reader = csv.reader(infile)
+                        header_lisst=next(reader)
+                        try:
+                            data=list(reader)
+                        except:
+                            sg.PopupError("error reading file")
+                        table = window.FindElement('tab')
+                        table.Update(values=data)
+                '''        
+                '''
+                
                 # clear the table
                 [window.FindElement((i,j)).Update('') for j in range(MAX_COL) for i in range(MAX_ROWS)]
 
@@ -160,31 +183,54 @@ def Main():
                         #no work no do
                         except:
                             pass
-                '''
+            '''
+                
         #TODO fix the save function to allow a user to change a field and save it as a csv
         #FIXME saves in an unreadable corrupt CSV
-        elif event == 'Save':
+        elif event1 == 'Save':
             filename = sg.PopupGetFile('filename to open', save_as = True,no_window=True, file_types=(("CSV Files","*.csv"),))
             for i, row in enumerate(data):
                 for j, item in enumerate(row):
                     location = (i,j)
                     try:
-                        target_element = window.FindElement(location)
+                        target_element = window1.FindElement(location)
                     except:
                         pass
-                    deform = values[target_element]
+                    deform = values1[target_element]
                     print (deform)
+        elif event1 == "Preview":
+            if not window2_active:
+                window2_active=True
+                if filename is not None:
+                    with open(filename, 'r') as infile:
+                        reader = csv.reader(infile)
+                        header_lisst = next(reader)
+                        data = list(reader)
+                layout2 = [[sg.Table(values = data,
+                                        headings = header_lisst,
+                                        auto_size_columns = False,
+                                        max_col_width = 15,
+                                        justification = 'right',
+                                        alternating_row_color = 'lightblue',
+                                        vertical_scroll_only = False,
+                                        num_rows = min(len(data),20))]]
 
+                window2 = sg.Window('Window2').Layout(layout2)
+            if window2_active:
+                event2, values2 = window2.Read(timeout=100)
+                if event2 is None or event2 == 'Exit':
+                    window2_active = False
+                    window2.Close()
 
         #if the user decides to select line graph
-        elif event == 'line':
+        elif event1 == 'line':
 
             #names everything for use in the rest of the program            
-            name = values['graphtitle']
-            plotwidthfloat= values['width']
-            plotheightfloat= values['height']
-            xaxisname = values['xlabel']
-            yaxisname = values['ylabel']
+            name = values1['graphtitle']
+            plotwidthfloat= values1['width']
+            plotheightfloat= values1['height']
+            xaxisname = values1['xlabel']
+            yaxisname = values1['ylabel']
 
             #changes the type floats to type Ints
             plotwidth=int(plotwidthfloat)
@@ -194,18 +240,18 @@ def Main():
             weight = pd.read_csv(filename)
 
             #gets the values of the headers
-            xhead = values['xheaders']
-            yhead = values['yheaders']
+            xhead = values1['xheaders']
+            yhead = values1['yheaders']
 
             #array of colors to choose from
             #TODO add more colors
             colorar = ['green','blue','red','orange','aqua','black', 'pink', 'cyan']
 
             #headers.Update(header_list)
-            if values['ASR'] == True:
+            if values1['ASR'] == True:
                 p = figure(title = name, plot_width=plotwidth, plot_height=plotheight, match_aspect=True)
-            if values['ASR'] == False:
-                p = figure(title = name, plot_width=plotwidth, plot_height=plotheight)
+            if values1['ASR'] == False:
+                p = figure(title = name, plot_width=plotwidth, plot_height=plotheight,tools='')
 
             p.xaxis.axis_label = xaxisname
             p.yaxis.axis_label = yaxisname
@@ -238,7 +284,7 @@ def Main():
             output_file(name+'.html')
 
             #grabbing the data from the legend area spot
-            lloc = values['legendloc']
+            lloc = values1['legendloc']
             
             #user selected location of legend
             if lloc == 'Top Right':
@@ -253,22 +299,41 @@ def Main():
             elif lloc == 'Bottom Left':
                 p.legend.location ="bottom_left" 
             #show the graph
+            '''
+            p.add_tools(HoverTool(
+                tooltips=[
+                    ( 'date',   '@date{%F}'            ),
+                    ( 'close',  '$@{adj close}{%0.2f}' ), # use @{ } for field names with spaces
+                    ( 'volume', '@volume{0.00 a}'      ),
+                ],
+
+                formatters={
+                    'date'      : 'datetime', # use 'datetime' formatter for 'date' field
+                    'adj close' : 'printf',   # use 'printf' formatter for 'adj close' field
+                                            # use default 'numeral' formatter for other fields
+                },
+
+                # display a tooltip whenever the cursor is vertically in line with a glyph
+                mode='vline'
+            ))   
+            '''        
+
             show(p)
 
         #if the user wants to reset the program
-        elif event == "Reset":
-                window.Close()
+        elif event1 == "Reset":
+                window1.Close()
                 Main()
 
-        elif event == 'Submit An Issue':
+        elif event1 == 'Submit An Issue':
             a_website = "https://github.com/Jobenland/CSV-Graph-Generator/issues"
             webbrowser.open_new(a_website)
 
         # if a valid table location entered, change that location's value
         try:
-            location = (int(values['inputrow']), int(values['inputcol']))
-            target_element = window.FindElement(location)
-            new_value = values['value']
+            location = (int(values1['inputrow']), int(values1['inputcol']))
+            target_element = window1.FindElement(location)
+            new_value = values1['value']
             if target_element is not None and new_value != '':
                 target_element.Update(new_value)
         except:
